8d8f4cf8162194e7fc5315c80c6b4e489335e5b0
Ayush Iyankan <ayushiyankan007@gmail.com>
2023-02-16 12:00:00 +0530

    add swagger docs

--- app.js
+++ app.js
@@ -3,11 +3,17 @@ require("express-async-errors");
 const express = require("express");
 const app = express();
 
-//security
+//extra
 const helmet = require("helmet");
 const cors = require("cors");
 const xss = require("xss-clean");
 const rateLimiter = require("express-rate-limit");
+
+//swagger
+const swaggerUI = require("swagger-ui-express");
+const yaml = require("yamljs");
+const swaggerDocument = yaml.load("./swagger.yaml");
+const { StatusCodes } = require("http-status-codes");
 
 // error handler
 const notFoundMiddleware = require("./middleware/not-found");
@@ -28,12 +34,22 @@ app.use(
     max: 100, // limit each IP to 100 requests per windowMs
   })
 );
+
+app.use(express.static("/"));
 app.use(express.json());
 app.use(helmet());
 app.use(cors());
 app.use(xss());
 
 // routes
+app.get("/", (req, res) => {
+  res
+    .status(StatusCodes.OK)
+    .send(
+      '<div><h1>Jobs API Docs</h1><a href="/api-docs">Check documentation</a></div>'
+    );
+});
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/jobs", auth, jobsRouter);
 
